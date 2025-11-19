914d2bc0a4ebf1a293cfec14a17b0cf5451f2bf7
mathews526 <santanamathew@ufl.edu>
2025-11-19 13:50:50 -0500

    Switched order items are printed out

--- src/Word.h
+++ src/Word.h
@@ -16,5 +16,6 @@ public:
     string getEnDef() const { return enDef; }
     string getRomaji() const { return romaji; }
     void displayWord() {
-        cout << "Kana: " << getKana() << ", Definition: " << getEnDef() << ", Romaji: " << getRomaji() << endl;}
+        cout << "Kana: " << getKana() << ", Romaji: " << getRomaji() << ", Definition: " << getEnDef() << endl;
+    }
 };
